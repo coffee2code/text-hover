883e8eb9f1437ad66773ecfc8faca4b402c2fc87
Scott Reilly <scott@coffee2code.com>
2020-01-07 21:49:55 -0800

    Update plugin framework to 049
    * Correct last arg in call to `add_settings_field()` to be an array
    * Wrap help text for settings in `label` instead of `p`
    * Only use `label` for help text for checkboxes, otherwise use `p`
    * Ensure a `textarea` displays as a block to prevent orphaning of subsequent help text
    * Note compatibility through WP 5.1+
    * Update copyright date (2019)

--- text-hover.php
+++ text-hover.php
@@ -55,7 +55,7 @@ if ( ! class_exists( 'c2c_TextHover' ) ) :
 
 require_once( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'c2c-plugin.php' );
 
-final class c2c_TextHover extends c2c_TextHover_Plugin_048 {
+final class c2c_TextHover extends c2c_TextHover_Plugin_049 {
 
 	/**
 	 * Name of plugin's setting.
